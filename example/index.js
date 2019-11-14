a013ed58f3a7c1306af00ec77171295ef01ac1f7
Swapnil <swapnil@DEV-C02V80Q0HV2Q.local>
2019-11-14 16:23:11 +0100

    initial carousel version

---
+++ example/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Carousel } from "../lib/index";
+
+const images = [
+  "https://via.placeholder.com/350x350",
+  "https://via.placeholder.com/350x350",
+  "https://via.placeholder.com/350x350"
+];
+
+function App() {
+  return (
+    <div>
+      <Carousel images={images} />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
